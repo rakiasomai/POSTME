82cf4713e7667c51e1156bdf88e45efcc2added0
rakiasomai <somai.rakia@hotmail.fr>
2021-05-05 17:19:26 +0100

    add changes

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
   );
   return (
     <Grid columns={3}>
-      <Grid.Row className="page-title">
+      <Grid.Row className="Header">
         <h1>Latest news</h1>
       </Grid.Row>
       <Grid.Row>
