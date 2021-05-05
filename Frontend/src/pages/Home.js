28f32a58a7c9c7fe6f37d054f100ad63cf1789ff
khalilholberton <1071@holbertonschool.com>
2021-05-05 17:03:15 +0100

    fix

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
   return (
     <Grid columns={3}>
       <Grid.Row className="page-title">
-        <h1>Recent Posts</h1>
+        <h1>Latest news</h1>
       </Grid.Row>
       <Grid.Row>
         {user && (
